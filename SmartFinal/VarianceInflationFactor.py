66e1ca1c4baee167402e280e65e40bfba6d216e4
mbonkoungou <153067288+mbonkoungou@users.noreply.github.com>
2025-05-13 23:36:24 +0200

    Add files via upload

---
+++ SmartFinal/VarianceInflationFactor.py
@@ -0,0 +1,29 @@
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+import statsmodels.api as sm
+import pandas as pd
+
+def vif_score(path):
+    """
+    Renvoie un score de redondance basé sur les VIF :
+        - 1.0 si toutes les colonnes ont VIF < 5
+        - 0.5 si aucune ≥ 10 mais au moins une entre [5, 10[
+        - 0.0 si au moins une colonne a un VIF ≥ 10
+    """
+
+    dataset = pd.read_csv(path)
+
+    X_feat = dataset.drop(columns=dataset.columns[-1])
+
+    Xc = sm.add_constant(X_feat)
+    vifs = pd.Series(
+        [variance_inflation_factor(Xc.values, i) for i in range(1, Xc.shape[1])],
+        index=X_feat.columns
+    )
+
+    if (vifs >= 10).any():
+        return 0.0
+    elif (vifs >= 5).any():
+        return 0.5
+    else:
+        return 1.0
+
